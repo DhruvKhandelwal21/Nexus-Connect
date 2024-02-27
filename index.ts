2011e8e021f6a3dc0c74d31ebe35e03f861b33c4
dhruv@dk <dhruv21.dk@gmail.com>
2024-02-27 22:41:41 +0530

    setup complete

--- index.ts
+++ index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
+//The * symbol indicates that all exports should be imported, 
+//and as dotenv renames the imported module to dotenv.
+import * as dotenv from "dotenv";
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+
+console.log(process.env.PORT)
 
 const PORT = process.env.PORT || 5000;
 
